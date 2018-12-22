d9515de51edf67234113601b0f872f80df393a88
eddiejibson <jibson@tuta.io>
2018-12-22 23:48:06 +0000

    started to implement some suggestions

--- src/limitrr.php
+++ src/limitrr.php
@@ -3,7 +3,7 @@
  * @Project: limitrr-php
  * @Created Date: Tuesday, December 11th 2018, 10:23:30 am
  * @Author: Edward Jibson
- * @Last Modified Time: December 21st 2018, 7:27:47 pm
+ * @Last Modified Time: December 22nd 2018, 11:46:41 pm
  * @Last Modified By: Edward Jibson
  */
 namespace eddiejibson\limitrr;
@@ -17,44 +17,43 @@ class Limitrr
         }
         if (isset($conf["options"]["mw"]) && $conf["options"]["mw"]) {
             $this->options = [
-                "keyName" => (isset($conf["options"]["keyName"]) ? $conf["options"]["keyName"] : "limitrr"),
+                "keyName" => $conf["options"]["keyName"] ?? "limitrr",
             ];
         } else {
             $conf["routes"]["default"] = [
-                "requestsPerExpiry" => (isset($conf["routes"]["default"]["requestsPerExpiry"]) ? $conf["routes"]["default"]["requestsPerExpiry"] : 100),
-                "completedActionsPerExpiry" => (isset($conf["routes"]["default"]["completedActionsPerExpiry"]) ? $conf["routes"]["default"]["completedActionsPerExpiry"] : 5),
-                "expiry" => (isset($conf["routes"]["default"]["expiry"]) ? $conf["routes"]["default"]["expiry"] : 900),
-                "completedExpiry" => (isset($conf["routes"]["default"]["completedExpiry"]) ? $conf["routes"]["default"]["completedExpiry"] : 900),
+                "requestsPerExpiry" => $conf["routes"]["default"]["requestsPerExpiry"] ?? 100,
+                "completedActionsPerExpiry" => $conf["routes"]["default"]["completedActionsPerExpiry"] ?? 5,
+                "expiry" => $conf["routes"]["default"]["expiry"] ?? 900,
+                "completedExpiry" => $conf["routes"]["default"]["completedExpiry"] ?? 900,
                 "errorMsgs" => [
-                    "requests" => (isset($conf["routes"]["default"]["errorMsgs"]["requests"]) ? $conf["routes"]["default"]["errorMsgs"]["requests"] : "As you have made too many requests, you have been rate limited."),
-                    "actions" => (isset($conf["routes"]["default"]["errorMsgs"]["completed"]) ? $conf["routes"]["default"]["errorMsgs"]["completed"] : "As you performed too many successful actions, you are being rate limited."),
+                    "requests" => $conf["routes"]["default"]["errorMsgs"]["requests"] ?? "As you have made too many requests, you have been rate limited.",
+                    "actions" => $conf["routes"]["default"]["errorMsgs"]["completed"] ?? "As you performed too many successful actions, you are being rate limited.",
                 ],
             ];
             $this->options = [
-                "keyName" => (isset($conf["options"]["keyName"]) ? $conf["options"]["keyName"] : "limitrr"),
-                "errorStatusCode" => (isset($conf["options"]["errorStatusCode"]) ? $conf["options"]["errorStatusCode"] : 429),
+                "keyName" => $conf["options"]["keyName"] ?? "limitrr",
+                "errorStatusCode" => $conf["options"]["errorStatusCode"] ?? 429,
             ];
             $this->routes = $this->setDefaultToUndefined($conf["routes"], $conf["routes"]["default"]);
         }
-        return $this->connect($conf["redis"]);
+        $this->connect($conf["redis"]);
     }
 
     public function __destruct()
     {
         $this->db = null;
-        return true;
     }
 
     private function connect(array $conf = [])
     {
         try {
             if (!isset($conf["uri"])) {
-                $conf["host"] = (isset($conf["host"]) ? $conf["host"] : "127.0.0.1");
-                $conf["port"] = (isset($conf["port"]) ? $conf["port"] : 6379);
+                $conf["host"] = $conf["host"] ?? "127.0.0.1";
+                $conf["port"] = $conf["port"] ?? 6379;
             } else {
-                $conf = strval($conf["uri"]);
+                $conf = (string)$conf["uri"];
             }
-            $conf["database"] = (isset($conf["database"]) ? $conf["database"] : 0);
+            $conf["database"] = $conf["database"] ?? 0;
             $this->db = $client = new \Predis\Client($conf);
             $client->connect();
         } catch (\Predis\Connection\ConnectionException $e) {
@@ -63,9 +62,9 @@ class Limitrr
         }
     }
 
-    public function limit($opts = [], $req, $res, $next)
+    public function limit(array $opts = [], $req, $res, $next)
     {
-        $route = (isset($opts["route"]) ? $opts["route"] : "default");
+        $route = $opts["route"] ?? "default";
         if ($req->hasHeader("CF-Connecting-IP")) {
             $ip = $req->getHeader("CF-Connecting-IP");
         } elseif ($req->hasHeader("X-Forwarded-For")) {
@@ -76,11 +75,12 @@ class Limitrr
             $ip = $req->getServerParam('REMOTE_ADDR');
         }
         $keyName = $this->options["keyName"];
+        $key = "limitrr:${keyName}:${discriminator}:${route}";
         $result = $this->db->pipeline()
-            ->get("limitrr:${keyName}:${ip}:${route}:requests")
-            ->get("limitrr:${keyName}:${ip}:${route}:completed")
-            ->ttl("limitrr:${keyName}:${ip}:${route}:requests")
-            ->ttl("limitrr:${keyName}:${ip}:${route}:completed")
+            ->get("${key}:requests")
+            ->get("${key}:completed")
+            ->ttl("${key}:requests")
+            ->ttl("${key}:completed")
             ->execute();
         if ($result) {
             if (!$result[0]) {
@@ -105,7 +105,7 @@ class Limitrr
                 ], $this->options["errorStatusCode"]);
             } elseif ($result[0] > -2) {
                 try {
-                    $result = $this->db->incr("limitrr:${keyName}:${ip}:${route}:requests");
+                    $result = $this->db->incr("${key}:requests");
                     return $next($req, $res);
                 } catch (\Exception $e) {
                     $this->handleError($e);
@@ -113,8 +113,8 @@ class Limitrr
             } else {
                 try {
                     $result = $this->db->pipeline()
-                        ->incr("limitrr:${keyName}:${ip}:${route}:requests")
-                        ->expire("limitrr:${keyName}:${ip}:${route}:requests", $this->routes[$route]["requestsPerExpiry"])
+                        ->incr("${key}:requests")
+                        ->expire("${key}:requests", $this->routes[$route]["requestsPerExpiry"])
                         ->execute();
                     return $next($req, $res);
                 } catch (\Exception $e) {
@@ -124,15 +124,16 @@ class Limitrr
         }
     }
 
-    public function complete(array $opts)
+    public function complete(array $opts) : int
     {
         $keyName = $this->options["keyName"];
         $discriminator = $opts["discriminator"];
-        $route = (isset($opts["route"]) ? $opts["route"] : "default");
-        $currentResult = $this->db->get("limitrr:${keyName}:${discriminator}:${route}:completed");
+        $route = $opts["route"] ?? "default";
+        $key = "limitrr:${keyName}:${discriminator}:${route}";
+        $currentResult = $this->db->get("${key}:completed");
         if ($currentResult) {
             try {
-                $result = $this->db->incr("limitrr:${keyName}:${discriminator}:${route}:completed");
+                $result = $this->db->incr("${key}:completed");
             } catch (\Exception $e) {
                 $this->handleError($e);
             }
@@ -144,8 +145,8 @@ class Limitrr
         } else {
             try {
                 $result = $this->db->pipeline()
-                    ->incr("limitrr:${keyName}:${discriminator}:${route}:completed")
-                    ->expire("limitrr:${keyName}:${discriminator}:${route}:requests", $this->routes[$route]["completedActionsPerExpiry"])
+                    ->incr("${key}:completed")
+                    ->expire("${key}:requests", $this->routes[$route]["completedActionsPerExpiry"])
                     ->execute();
             } catch (\Exception $e) {
                 $this->handleError($e);
@@ -158,45 +159,49 @@ class Limitrr
         }
     }
 
-    public function get(array $opts)
+    public function get(array $opts) : array
     {
         $keyName = $this->options["keyName"];
         $discriminator = $opts["discriminator"];
-        $route = (isset($opts["route"]) ? $opts["route"] : "default");
+        $route = $opts["route"] ?? "default";
+        $key = "limitrr:${keyName}:${discriminator}:${route}";
         if (!isset($opts["type"])) {
             try {
                 $result = $this->db->pipeline()
-                    ->get("limitrr:${keyName}:${discriminator}:${route}:requests")
-                    ->get("limitrr:${keyName}:${discriminator}:${route}:completed")
+                    ->get("${key}:requests")
+                    ->get("${key}:completed")
                     ->execute();
             } catch (\Exception $e) {
                 $this->handleError($e);
             }
             return [
-                "requests" => ($result[0] ? intval($result[0]) : 0),
-                "completed" => ($result[1] ? intval($result[1]) : 0),
+                "requests" => (int)($result[0] ?? 0),
+                "completed" => (int)($result[1] ?? 0)
             ];
         } else {
             $type = $opts["type"];
             try {
-                $result = $this->db->get("limitrr:${keyName}:${discriminator}:${route}:${type}");
+                $result = $this->db->get("${key}:${type}");
             } catch (\Exception $e) {
                 $this->handleError($e);
             }
-            return ($result ? intval($result) : 0);
+            return [
+                (string)$type => (int)($result ?? 0)
+            ];
         }
     }
 
-    public function reset(array $opts)
+    public function reset(array $opts) : bool
     {
         $keyName = $this->options["keyName"];
         $discriminator = $opts["discriminator"];
-        $route = (isset($opts["route"]) ? $opts["route"] : "default");
+        $route = $opts["route"] ?? "default";
+        $key = "limitrr:${keyName}:${discriminator}:${route}";
         if (!isset($opts["type"])) {
             try {
                 $result = $this->db->pipeline()
-                    ->del("limitrr:${keyName}:${discriminator}:${route}:requests")
-                    ->del("limitrr:${keyName}:${discriminator}:${route}:completed")
+                    ->del("${key}:requests")
+                    ->del("${key}:completed")
                     ->execute();
             } catch (\Exception $e) {
                 $this->handleError($e);
@@ -209,7 +214,7 @@ class Limitrr
         } else {
             $type = $opts["type"];
             try {
-                $result = $this->db->del("limitrr:${keyName}:${discriminator}:${route}:${type}");
+                $result = $this->db->del("${key}:${type}");
             } catch (\Exception $e) {
                 $this->handleError($e);
             }
@@ -227,10 +232,10 @@ class Limitrr
         throw new \Exception("Limitrr : An error was encountered . ${msg} ", 1);
     }
 
-    private function setDefaultToUndefined(array $routes, array $default)
+    private function setDefaultToUndefined(array $routes, array $default) : array
     {
         foreach ($routes as $key => $value) {
-            if ($key != " default ") {
+            if ($key != "default") {
                 foreach ($default as $defaultKey => $defaultValue) {
                     if (!isset($routes[$key][$defaultKey])) {
                         $routes[$key][$defaultKey] = $defaultValue;
