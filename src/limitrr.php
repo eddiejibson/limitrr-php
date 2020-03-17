0a0e23e9a1fe41ef48d89dfb3c686101be0970bd
Edward Jibson <jibson@tuta.io>
2020-03-17 19:37:02 +0000

    Update limitrr.php

--- src/limitrr.php
+++ src/limitrr.php
@@ -38,6 +38,10 @@ class Limitrr
         }
         $this->db = $this->connect($conf["redis"]);
     }
+    
+    public static function getIp() {
+        return $_SERVER["HTTP_CF_CONNECTING_IP"] ?? $_SERVER['HTTP_CLIENT_IP'] ?? $_SERVER['HTTP_X_FORWARDED_FOR'] ?? $_SERVER['REMOTE_ADDR'] ?? null;
+    }
 
     public function __destruct()
     {
